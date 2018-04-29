ef99b973a278924d296fec95c13e77a2882c4040
CodeAll20 <37627810+CodeAll20@users.noreply.github.com>
2018-04-29 16:00:20 -0400

    Update r.java

--- r.java
+++ r.java
@@ -16,8 +16,20 @@ abstract public class r
    
    public void draw (double angleFrom, int angleTo, DrawingTool Pen, SketchPad Paper)
    {
-      for (double i = angleFrom; i <= angleTo; i += (angleTo - angleFrom) / (2 * Math.PI * 100))
-         pen.draw
+      //Here is the actual movement of the pen
+      pen.up();
+      for (double theta = angleFrom; theta <= angleTo; theta += (angleTo - angleFrom) / (2 * Math.PI * 200))
+      {
+         /*
+         Since x = r * sin(theta) and y = r * cos(theta),
+         I made the program print the return from the method r (defined below), and the multiplied it by sin or cos, depending on
+         whether it was x or y.
+         I also multiplied the values by 100, to make the graph larger. We can deal wth scaling later
+         */
+         pen.move((getValue(theta) * Math.cos(theta) * 100), (getValue(theta) * Math.sin(theta) * 100));
+         if (!pen.isDown())
+            pen.down();
+      }
    }
    
    public double getZero();
