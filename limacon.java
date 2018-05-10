18f24654039dd644cbc1b0f4876e90c66b008802
CodeAll20 <37627810+CodeAll20@users.noreply.github.com>
2018-05-10 13:58:16 -0400

    Update limacon.java

--- limacon.java
+++ limacon.java
@@ -1,26 +1,16 @@
 import java.util.*;
 import apcslib.*;
 
-public class limacon extends r
-{
-   private int a;
-   private int b;
-   private boolean isSin;
-   
+public class limacon extends rose
+{   
    public limacon (DrawingTool Pen, SketchPad Paper)
    {
       super(Pen, Paper);
-      a = 1;
-      b = 2;
-      isSin = true;
    }
    
    public limacon (DrawingTool Pen, SketchPad Paper, int A, int B, boolean IsSin)
    {
-      super(Pen, Paper);
-      a = A;
-      b = B;
-      isSin = IsSin;
+      super(Pen, Paper, A, B, IsSin);
    }
    
    public double getValue (double theta)
@@ -28,25 +18,30 @@ public class limacon extends r
       double value = 0;
       
       if (isSin)
-         value = b + a * Math.sin(theta);
+         value = getB() + getA() * Math.sin(theta);
       else
-         value = b + a * Math.cos(theta);
+         value = getB() + getA() * Math.cos(theta);
       
       return value;
    }
    
    public String toString ()
    {
-      String Return = "";                      
+      String Return = "";
       
-      if (Math.abs(b / a) >= 1)
+      if (getIsSin())
+         Return += "Intersects y-axis at y = 0 and y = " + (a - b) + "\n";
+      else
+         Return += "Intersects x-axis at x = 0 and x = " + (a - b) + "\n";
+      
+      if (Math.abs(getB() / getA()) >= 1)
          Return += "Has no dip or loop\n";
-      else if (Math.abs(b / a) == 1)
+      else if (Math.abs(getB() / getA()) == 1)
          Return += "Has a dip\n";
       else
          Return += "Has a loop\n";
       
-      if (isSin)
+      if (getIsSin())
          Return += "Alignment: y-axis\n";
       else
          Return += "Alignment: x-axis\n";
