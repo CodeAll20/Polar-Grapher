65434dbc6a97674a8ea94eee4acedee64ba42ac9
CodeAll20 <37627810+CodeAll20@users.noreply.github.com>
2018-05-10 10:46:15 -0400

    Update Function.java

--- Function.java
+++ Function.java
@@ -3,7 +3,6 @@ import apcslib.*;
 
 public interface Function
 {
-  public void flipSign();
   public double getValue(double theta);
   public void draw (double angleFrom, int angleTo);
   public String toString();
