94c939308034600973639f767807e7afd270e904
CodeAll20 <37627810+CodeAll20@users.noreply.github.com>
2018-05-10 10:50:43 -0400

    Add files via upload

---
+++ spiral.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import apcslib.*;
+
+public class spiral extends r
+{
+   private int a;
+   private boolean isCsc;
+   
+   public spiral (DrawingTool Pen, SketchPad Paper)
+   {
+      super(Pen, Paper);
+      a = 1;
+      isCsc = true;
+   }
+   
+   public spiral (DrawingTool Pen, SketchPad Paper, int A, boolean IsCsc)
+   {
+      super(Pen, Paper);
+      a = A;
+      isCsc = IsCsc;
+   }
+   
+   public double getValue (double theta)
+   {
+      double value = 0;
+      
+      if (isCsc)
+         value = a / Math.sin(theta);
+      else
+         value = a / Math.cos(theta);
+      
+      return value;
+   }
+   
+   public String toString ()
+   {
+      String Return = "";
+      
+      if (isCsc)
+         Return += "Parallel with x-axis\n" +
+                   "Touches y-axis at " + a + "\n";
+      else
+         Return += "Parallel with y-axis\n" +
+                   "Touches x-axis at " + a + "\n";
+      
+      return Return;
+   }
+}
