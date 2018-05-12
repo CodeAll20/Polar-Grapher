da55f4abcd78bedd79a2f56616287d6ad91a45fd
taom19 <37919550+taom19@users.noreply.github.com>
2018-05-11 21:42:45 -0400

    Update radGrapherRBMT.java

--- radGrapherRBMT.java
+++ radGrapherRBMT.java
@@ -1,43 +1,97 @@
 import java.util.*;
 import apcslib.*;
+import java.awt.*;
 
 public class radGrapherRBMT
 {
-   //These are all the instance variables
-   private static DrawingTool pen;
-   private static SketchPad paper;
-   private static double theta;
+   public static Scanner sc = new Scanner(System.in);
+   private static DrawingTool Pen;
+   private static  SketchPad Paper;
    
-   //This is the main method, where all the graphing will happen
-   public static void main (String [] args)
+   public static void main (String[] args)
    {
-      //Clearing the instance variables
-      paper = new SketchPad(1000, 700, 0);
-      pen = new DrawingTool(paper);
-      theta = 0;
+      //instance variables
+      int pick = 0; //initialize switch
+      Paper = new SketchPad(1920, 1080, 0);
+      Pen = new DrawingTool(Paper);
       
-      //Here is the actual movement of the pen
-      pen.up();
-      for (theta = 0; theta <= 2 * Math.PI; theta += 2 * Math.PI / 200)
-      {
-         /*
-         Since x = r * sin(theta) and y = r * cos(theta),
-         I made the program print the return from the method r (defined below), and the multiplied it by sin or cos, depending on
-         whether it was x or y.
-         I also multiplied the values by 100, to make the graph larger. We can deal wth scaling later
-         */
-         pen.move((int)(r(theta) * Math.cos(theta) * 100), (int)(r(theta) * Math.sin(theta) * 100));
-         if (!pen.isDown())
-            pen.down();
-      }
-   }
+      ArrayList<r> funList = new <r>ArrayList();
    
-   /*
-   Here is the method that actually returns values for the main method to print. I am thinking of making the r emthod into a full-on
-   class later on
-   */
-   private static double r (double angle)
-   {
-      return 1 - 2 * Math.sin(theta);
-   }
+      //array of colors that pen can change into
+      String[] colors = new String[]{"Blue", "Green", "Red", "Yellow", "Create"} ; //create creates a new color based on rbg values entered
+      int r = 0;
+      int b = 0;
+      int g = 0;
+      String cPick = "";
+      
+      r r1 = new circle (Pen, Paper, 10, '1');
+      
+      funList.add(r1);
+      r1.draw(-5.5, 5);
+     
+      /*for (int i = 0; i < funArray.length; i++)
+      funArray[i] = new circle (Pen, Paper, 1, '1');*/
+      
+     /* for(r rs: funArray) 
+      {
+      rs.draw(-55.5, 55);
+      }*/
+      
+      
+      while(pick < 6)
+      {
+         System.out.println(" Welcome to Polar Grapher!");
+         System.out.println(" What would you like to do?: ");
+         System.out.println(" 1 - Look at examples of functions");
+         System.out.println(" 2 - Graph a function ");
+         System.out.println(" 3 - Add a new function "); //(Add a new function to array)
+         System.out.println(" 4 - Change color of the pen ");
+         System.out.println(" 5 - Graph all the functions! "); //(Graph all functions in the array)
+         System.out.println(" 6 - Exit ");
+       
+           
+         pick = sc.nextInt(); //scanner for keyboard input
+         switch(pick)
+         {
+         case 1:
+         case 2:
+         case 3:
+         case 4: //change pen's color
+            System.out.println(" Press enter and pick (type) a color");
+            sc.nextLine();
+            System.out.println(colors);
+            cPick = sc.nextLine();
+               if(cPick == "Blue")
+                  Pen.setColor(Color.blue);
+               else if(cPick == "Green")
+                  Pen.setColor(Color.green);
+               else if(cPick == "Red")
+                  Pen.setColor(Color.red);
+               else if(cPick == "Yellow")
+                  Pen.setColor(Color.yellow);
+               else if(cPick == "Create")
+               {
+                  System.out.println("You can create a new color by entering RGB values");
+                  sc.nextLine();
+                  System.out.println("Please enter R value: ");
+                  r = Integer.parseInt(sc.nextLine());
+                  System.out.println("Please enter G value: ");
+                  g = Integer.parseInt(sc.nextLine());
+                  System.out.println("Please enter B value: ");
+                  b = Integer.parseInt(sc.nextLine());
+                  System.out.println("Congrats you have created a new color!");
+                  Pen.setColor(new Color(r, g, b));
+               }
+             break;  
+            
+         case 5:
+         case 6:
+            System.out.println("Thanks for graphing!");
+            System.exit(0); //ends program
+            break;
+         }
+      
+      } 
+   }   
+   
 }
